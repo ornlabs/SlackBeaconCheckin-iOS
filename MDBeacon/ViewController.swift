ccec27bfb7429dda82a7fbfe5f5f8d638916e316
maxyel <max.yelsky@gmail.com>
2015-05-29 16:28:35 -0400

    Initial Commit

---
+++ MDBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MDBeacon
+//
+//  Created by Max Yelsky on 5/29/15.
+//  Copyright (c) 2015 MissionData. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
